5fec71daa3a7cb2637fca13f5e8ad318d1a2ffee
Guiioff <guilherme.rivaldy@upe.br>
2026-05-27 16:50:38 -0300

    feat: cria controller para o redirect das short urls

---
+++ src/main/java/com/devgui/urlshortener/api/controller/RedirectController.java
@@ -0,0 +1,32 @@
+package com.devgui.urlshortener.api.controller;
+
+import com.devgui.urlshortener.domain.model.ShortUrl;
+import com.devgui.urlshortener.domain.service.ShortUrlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping
+public class RedirectController {
+
+    private final ShortUrlService shortUrlService;
+
+    public RedirectController(ShortUrlService shortUrlService) {
+        this.shortUrlService = shortUrlService;
+    }
+
+    @GetMapping("/{shortKey}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortKey){
+        ShortUrl shortUrl = shortUrlService.getByShortKey(shortKey);
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(shortUrl.getOriginalUrl()))
+                .build();
+    }
+}
