2bdfcae3ec60e00d52eed6ad36b583d1cebb9339
Guiioff <guilherme.rivaldy@upe.br>
2026-05-27 16:50:07 -0300

    feat: cria controller para ShortUrl

---
+++ src/main/java/com/devgui/urlshortener/api/controller/ShortUrlController.java
@@ -0,0 +1,38 @@
+package com.devgui.urlshortener.api.controller;
+
+import com.devgui.urlshortener.api.dto.request.ShortUrlRequest;
+import com.devgui.urlshortener.api.dto.response.ShortUrlResponse;
+import com.devgui.urlshortener.api.mapper.ShortUrlMapper;
+import com.devgui.urlshortener.domain.model.ShortUrl;
+import com.devgui.urlshortener.domain.service.ShortUrlService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/v1/urls")
+public class ShortUrlController {
+
+    private final ShortUrlService shortUrlService;
+    private final ShortUrlMapper shortUrlMapper;
+
+    public ShortUrlController(ShortUrlService shortUrlService, ShortUrlMapper shortUrlMapper) {
+        this.shortUrlService = shortUrlService;
+        this.shortUrlMapper = shortUrlMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<ShortUrlResponse> shorten(@RequestBody @Valid ShortUrlRequest dto){
+        ShortUrl shortUrl = shortUrlMapper.toEntity(dto);
+        ShortUrl shortUrlSaved = shortUrlService.shorten(shortUrl);
+        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().toUriString();
+
+        ShortUrlResponse shortUrlResponse = shortUrlMapper.toResponse(shortUrlSaved, baseUrl);
+        return ResponseEntity.status(HttpStatus.CREATED).body(shortUrlResponse);
+    }
+}
