0f9d56358230a7f0e6c11e60428b576e2fa2971d
Guiioff <guilherme.rivaldy@upe.br>
2026-06-01 22:49:49 -0300

    feat: cria método para listagem paginada de ShortUrl

--- src/main/java/com/devgui/urlshortener/domain/service/ShortUrlService.java
+++ src/main/java/com/devgui/urlshortener/domain/service/ShortUrlService.java
@@ -5,6 +5,9 @@ import com.devgui.urlshortener.domain.exception.ShortUrlNotFoundException;
 import com.devgui.urlshortener.domain.model.ShortUrl;
 import com.devgui.urlshortener.infrastructure.repository.ShortUrlRepository;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -42,6 +45,11 @@ public class ShortUrlService {
     public ShortUrl getById(UUID id){
         return shortUrlRepository.findById(id)
                 .orElseThrow(() -> new ShortUrlNotFoundException("Short URL not found for id: " + id));
+    }
+
+    public Page<ShortUrl> getAll(Integer page, Integer size){
+        Pageable pageRequest = PageRequest.of(page, size);
+        return shortUrlRepository.findAll(pageRequest);
     }
 
     private ShortUrl getByShortKey(String shortKey){
