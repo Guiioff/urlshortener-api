1516fb71d78f2d4ac3c894d6cd321969b817225b
Guiioff <guilherme.rivaldy@upe.br>
2026-06-01 22:52:08 -0300

    feat: cria rota GET para listagem paginada de ShortUrl

--- src/main/java/com/devgui/urlshortener/api/controller/ShortUrlController.java
+++ src/main/java/com/devgui/urlshortener/api/controller/ShortUrlController.java
@@ -1,12 +1,16 @@
 package com.devgui.urlshortener.api.controller;
 
 import com.devgui.urlshortener.api.dto.request.ShortUrlRequest;
+import com.devgui.urlshortener.api.dto.response.PageResponse;
 import com.devgui.urlshortener.api.dto.response.ShortUrlDetailsResponse;
 import com.devgui.urlshortener.api.dto.response.ShortUrlResponse;
 import com.devgui.urlshortener.api.mapper.ShortUrlMapper;
 import com.devgui.urlshortener.domain.model.ShortUrl;
 import com.devgui.urlshortener.domain.service.ShortUrlService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -45,4 +49,19 @@ public class ShortUrlController {
         return ResponseEntity.ok(shortUrlResponse);
     }
 
+    @GetMapping
+    public ResponseEntity<PageResponse<ShortUrlResponse>> getAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false)
+            @Min(value = 0, message = "Page must be greater than or equal to 0")
+            Integer page,
+            @RequestParam(value = "size", defaultValue = "5", required = false)
+            @Max(value = 20, message = "Size must not exceed 20")
+            Integer size
+    ){
+        Page<ShortUrl> shortUrlPage = shortUrlService.getAll(page, size);
+        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().toUriString();
+
+        PageResponse<ShortUrlResponse> response = shortUrlMapper.toPageResponse(shortUrlPage, baseUrl);
+        return ResponseEntity.ok(response);
+    }
 }
