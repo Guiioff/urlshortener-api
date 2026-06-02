0f44e180e6906b69b4434d41e200022e8aec3538
Guiioff <guilherme.rivaldy@upe.br>
2026-06-01 21:49:58 -0300

    feat: cria rota GET para retornar uma ShortUrl pelo ID

--- src/main/java/com/devgui/urlshortener/api/controller/ShortUrlController.java
+++ src/main/java/com/devgui/urlshortener/api/controller/ShortUrlController.java
@@ -1,6 +1,7 @@
 package com.devgui.urlshortener.api.controller;
 
 import com.devgui.urlshortener.api.dto.request.ShortUrlRequest;
+import com.devgui.urlshortener.api.dto.response.ShortUrlDetailsResponse;
 import com.devgui.urlshortener.api.dto.response.ShortUrlResponse;
 import com.devgui.urlshortener.api.mapper.ShortUrlMapper;
 import com.devgui.urlshortener.domain.model.ShortUrl;
@@ -8,11 +9,10 @@ import com.devgui.urlshortener.domain.service.ShortUrlService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/urls")
@@ -35,4 +35,14 @@ public class ShortUrlController {
         ShortUrlResponse shortUrlResponse = shortUrlMapper.toResponse(shortUrlSaved, baseUrl);
         return ResponseEntity.status(HttpStatus.CREATED).body(shortUrlResponse);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ShortUrlDetailsResponse> getById(@PathVariable UUID id){
+        ShortUrl shortUrl = shortUrlService.getById(id);
+        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().toUriString();
+
+        ShortUrlDetailsResponse shortUrlResponse = shortUrlMapper.toDetailsResponse(shortUrl, baseUrl);
+        return ResponseEntity.ok(shortUrlResponse);
+    }
+
 }
