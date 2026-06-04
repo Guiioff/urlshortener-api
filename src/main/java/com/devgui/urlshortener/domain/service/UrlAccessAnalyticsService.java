c5693672c8a1dfb847537457739041db5b271857
Guiioff <guilherme.rivaldy@upe.br>
2026-06-04 20:11:25 -0300

    feat: implementa método para retornar uma lista de UrlAccessAnalytics pelo ID do ShortUrl

--- src/main/java/com/devgui/urlshortener/domain/service/UrlAccessAnalyticsService.java
+++ src/main/java/com/devgui/urlshortener/domain/service/UrlAccessAnalyticsService.java
@@ -3,7 +3,12 @@ package com.devgui.urlshortener.domain.service;
 import com.devgui.urlshortener.domain.model.ShortUrl;
 import com.devgui.urlshortener.domain.model.UrlAccessAnalytics;
 import com.devgui.urlshortener.infrastructure.repository.UrlAccessAnalyticsRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UrlAccessAnalyticsService {
@@ -17,5 +22,10 @@ public class UrlAccessAnalyticsService {
     public void create(String userAgent, String referrer, ShortUrl shortUrl){
         UrlAccessAnalytics urlAccessAnalytics = new UrlAccessAnalytics(userAgent, referrer, shortUrl);
         urlAccessAnalyticsRepository.save(urlAccessAnalytics);
+    }
+
+    public List<UrlAccessAnalytics> getAnalytics(UUID shortUrlId){
+        Pageable pageable = PageRequest.of(0, 5);
+        return urlAccessAnalyticsRepository.findRecentAccessesByShortUrlId(shortUrlId, pageable);
     }
 }
--- src/main/java/com/devgui/urlshortener/infrastructure/repository/UrlAccessAnalyticsRepository.java
+++ src/main/java/com/devgui/urlshortener/infrastructure/repository/UrlAccessAnalyticsRepository.java
@@ -1,9 +1,19 @@
 package com.devgui.urlshortener.infrastructure.repository;
 
 import com.devgui.urlshortener.domain.model.UrlAccessAnalytics;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UrlAccessAnalyticsRepository extends JpaRepository<UrlAccessAnalytics, UUID> {
+    @Query("""
+    SELECT uaa FROM UrlAccessAnalytics uaa
+    WHERE uaa.shortUrl.id = :id
+    ORDER BY uaa.accessedAt DESC
+    """)
+    List<UrlAccessAnalytics> findRecentAccessesByShortUrlId(@Param("id") UUID id, Pageable pageable);
 }
