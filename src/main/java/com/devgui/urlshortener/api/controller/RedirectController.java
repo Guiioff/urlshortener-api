fd03e2d075e2232787d9de7c9934107ec9684625
Guiioff <guilherme.rivaldy@upe.br>
2026-06-04 19:01:47 -0300

    feat: implementa criação de estatísticas de acesso ao redirecionar uma url

--- src/main/java/com/devgui/urlshortener/api/controller/RedirectController.java
+++ src/main/java/com/devgui/urlshortener/api/controller/RedirectController.java
@@ -2,6 +2,8 @@ package com.devgui.urlshortener.api.controller;
 
 import com.devgui.urlshortener.domain.model.ShortUrl;
 import com.devgui.urlshortener.domain.service.ShortUrlService;
+import com.devgui.urlshortener.domain.service.UrlAccessAnalyticsService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +18,20 @@ import java.net.URI;
 public class RedirectController {
 
     private final ShortUrlService shortUrlService;
+    private final UrlAccessAnalyticsService urlAccessAnalyticsService;
 
-    public RedirectController(ShortUrlService shortUrlService) {
+    public RedirectController(ShortUrlService shortUrlService, UrlAccessAnalyticsService urlAccessAnalyticsService) {
         this.shortUrlService = shortUrlService;
+        this.urlAccessAnalyticsService = urlAccessAnalyticsService;
     }
 
     @GetMapping("/{shortKey}")
-    public ResponseEntity<Void> redirect(@PathVariable String shortKey){
+    public ResponseEntity<Void> redirect(@PathVariable String shortKey, HttpServletRequest request){
         ShortUrl shortUrl = shortUrlService.incrementClicks(shortKey);
+
+        String userAgent = request.getHeader("User-Agent");
+        String referrer = request.getHeader("Referer");
+        urlAccessAnalyticsService.create(userAgent, referrer, shortUrl);
 
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(URI.create(shortUrl.getOriginalUrl()))
