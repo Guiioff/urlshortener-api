0299868960525f8a91cbf251de6f38c7f04dc7ec
Guiioff <guilherme.rivaldy@upe.br>
2026-06-04 20:12:45 -0300

    feat: cria mapper para UrlAccessAnalytics

---
+++ src/main/java/com/devgui/urlshortener/api/mapper/UrlAccessAnalyticsMapper.java
@@ -0,0 +1,29 @@
+package com.devgui.urlshortener.api.mapper;
+
+import com.devgui.urlshortener.api.dto.response.RecentClickResponse;
+import com.devgui.urlshortener.api.dto.response.UrlAnalyticsResponse;
+import com.devgui.urlshortener.domain.model.ShortUrl;
+import com.devgui.urlshortener.domain.model.UrlAccessAnalytics;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UrlAccessAnalyticsMapper {
+
+    public UrlAnalyticsResponse toResponse(
+            ShortUrl shortUrl, List<UrlAccessAnalytics> urlAccessAnalyticsList){
+        String shortKey = shortUrl.getShortKey();
+        String originalUrl = shortUrl.getOriginalUrl();
+        Integer clickCount = shortUrl.getClickCount();
+        List<RecentClickResponse> recentClicks = urlAccessAnalyticsList
+                .stream()
+                .map(uaa -> new RecentClickResponse(
+                        uaa.getUserAgent(),
+                        uaa.getReferrer(),
+                        uaa.getAccessedAt()))
+                .toList();
+
+        return new UrlAnalyticsResponse(shortKey, originalUrl, clickCount, recentClicks);
+    }
+}
